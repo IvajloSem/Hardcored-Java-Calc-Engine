346546ac3e9112d00034d05d11ca9f3d4bbe7e4e
semka <semka@semka>
2018-01-10 12:59:21 +0200

    Started working with arrays. Hard coded and useless for now. Will update in the future.

--- src/hardcodedcalcengine/HardCodedCalcEngine.java
+++ src/hardcodedcalcengine/HardCodedCalcEngine.java
@@ -3,22 +3,35 @@ package hardcodedcalcengine;
 public class HardCodedCalcEngine {
 
     public static void main(String[] args) {
-        double val1 = 100, val2 = 23, result = 0;
-        char opCode = 'a';
+        double[] val1 = {5.0d, 10.0d, 15.0d, 20.0d};
+        double[] val2 = {1.0d, 2.0d, 3.0d, 4.0d};
+        char[] opCode = {'a', 's', 'd', 'm'};
+        double[] result = new double [opCode.length];
         
-        if(opCode == 'a')
-            result = val1+val2;
-            else if(opCode == 's')
-                result = val1-val2;
-            else if(opCode == 'd') //if val2 is different from 0, then divide, otherwise return 0
-                result = val2 != 0 ? val1 / val2 : 0.0d;
-            else if(opCode == 'm')
-                result = val1*val2;
-            else{
-            System.out.println("Invalid opCode");
+        for(int i=0; i<opCode.length; i++){
+            switch(opCode[i]){
+                case 'a':
+                    result[i] = val1[i] + val2[i];
+                    break;
+                case 's' :
+                    result[i] = val1[i] - val2[i];
+                    break;
+                case 'd':
+                    result[i] = val2[i] != 0.0d ? val1[i] / val2[i] : 0.0d; 
+                    break;
+                case 'm' :
+                    result[i] = val1[i] * val2[i];
+                    break;
+                default :
+                System.out.print("Invalid opCode");
+                break;
+            }
+            
         }
-        
-        System.out.println(result);
+        for(double theResult : result){
+        System.out.print("result = ");
+        System.out.println(theResult);
+        }
     }
     
 }
