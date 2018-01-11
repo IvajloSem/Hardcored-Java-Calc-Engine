c0a7deaa5fc7c7a324dfa6f8cfeb8c65417c3e7f
semka <semka@semka>
2018-01-11 16:05:29 +0200

    Started working with arrays. Hard coded and useless for now. Will update in the future.

--- src/hardcodedcalcengine/HardCodedCalcEngine.java
+++ src/hardcodedcalcengine/HardCodedCalcEngine.java
@@ -4,25 +4,26 @@ public class HardCodedCalcEngine {
 
     public static void main(String[] args) {
         
+        // creates MathEquation instance 
         MathEquation[] equations = new MathEquation[4];
         equations[0] = create(100.0d, 200.0d, 'd');
         equations[1] = create(200.0d, 150.0d, 'a');
         equations[2] = create(300.0d, 100.0d, 's');
         equations[3] = create(400.0d, 50.0d, 'm');
         
-       
+        // loops through all MathEquations and executes them, then displays the result
         for(MathEquation equation : equations){
             equation.execute();
             System.out.print("result = ");
-            System.out.println(equation.result);
+            System.out.println(equation.getResult());
         }
     }
     
     public static MathEquation create (double val1, double val2, char opCode){
         MathEquation equation = new MathEquation();
-        equation.val1 = val1;
-        equation.val2 = val2;
-        equation.opCode = opCode;
+        equation.setVal1 (val1);
+        equation.setVal2 (val2);
+        equation.setOpCode (opCode);
         
         return equation;
     }
