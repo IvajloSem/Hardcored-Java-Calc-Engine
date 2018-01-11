28726ed9e849d68f0769b7fb54a771d34af0b045
semka <semka@semka>
2018-01-11 15:19:41 +0200

    Started working with arrays. Hard coded and useless for now. Will update in the future.

--- src/hardcodedcalcengine/HardCodedCalcEngine.java
+++ src/hardcodedcalcengine/HardCodedCalcEngine.java
@@ -3,35 +3,28 @@ package hardcodedcalcengine;
 public class HardCodedCalcEngine {
 
     public static void main(String[] args) {
-        double[] val1 = {5.0d, 10.0d, 15.0d, 20.0d};
-        double[] val2 = {1.0d, 2.0d, 3.0d, 4.0d};
-        char[] opCode = {'a', 's', 'd', 'm'};
-        double[] result = new double [opCode.length];
         
-        for(int i=0; i<opCode.length; i++){
-            switch(opCode[i]){
-                case 'a':
-                    result[i] = val1[i] + val2[i];
-                    break;
-                case 's' :
-                    result[i] = val1[i] - val2[i];
-                    break;
-                case 'd':
-                    result[i] = val2[i] != 0.0d ? val1[i] / val2[i] : 0.0d; 
-                    break;
-                case 'm' :
-                    result[i] = val1[i] * val2[i];
-                    break;
-                default :
-                System.out.print("Invalid opCode");
-                break;
-            }
-            
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100.0d, 200.0d, 'd');
+        equations[1] = create(200.0d, 150.0d, 'a');
+        equations[2] = create(300.0d, 100.0d, 's');
+        equations[3] = create(400.0d, 50.0d, 'm');
+        
+       
+        for(MathEquation equation : equations){
+            equation.execute();
+            System.out.print("result = ");
+            System.out.println(equation.result);
         }
-        for(double theResult : result){
-        System.out.print("result = ");
-        System.out.println(theResult);
-        }
+    }
+    
+    public static MathEquation create (double val1, double val2, char opCode){
+        MathEquation equation = new MathEquation();
+        equation.val1 = val1;
+        equation.val2 = val2;
+        equation.opCode = opCode;
+        
+        return equation;
     }
     
 }
